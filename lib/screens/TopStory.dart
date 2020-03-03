9ac113633795ca497ca50546112b627fb45059c7
Raymond Yegon <raymondyegon17@gmail.com>
2020-03-03 11:23:13 +0300

    customized the navbar

--- lib/screens/TopStory.dart
+++ lib/screens/TopStory.dart
@@ -8,35 +8,43 @@ class TopStory extends StatefulWidget {
 class _TopStoryState extends State<TopStory> {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () => Future.value(false),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          centerTitle: true,
-          // backgroundColor: Colors.white,
-          title: Text(
-            'New York Times',
-            style: TextStyle(
-              fontFamily: 'chomsky',
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 40,
+    return SafeArea(
+      child: WillPopScope(
+        onWillPop: () => Future.value(false),
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(100),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'The New York Times',
+                    style: TextStyle(
+                      fontFamily: 'chomsky',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 40,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                'counter',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  'counter',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
+            ),
           ),
         ),
       ),
